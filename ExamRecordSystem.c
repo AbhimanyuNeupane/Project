6dabda7b77aec2d93ca58c3ffe8c85f6e566cbb9
Abhimanyu Neupane <38075836+AbhimanyuNeupane@users.noreply.github.com>
2019-01-19 18:19:16 +0545

    Update ExamRecordSystem.c

--- ExamRecordSystem.c
+++ ExamRecordSystem.c
@@ -1,125 +1,135 @@
-// Students Entrance Record System
 
-
-//***************************************************************
-//                   HEADER FILE USED IN PROJECT
-//****************************************************************
 
 #include<stdio.h>
 #include<conio.h>
 #include<stdlib.h>
 
+void option();
+void showdata();
+void getdata();
+void get_info();
+void case_default();
+
 //***************************************************************
 //                   DECLARATION OF VARIABLES IN PROJECT
 //****************************************************************
-
     int id,s_marks,e_marks,m_marks,n_marks;
     char name[20],address,grade;
+    char a[0];
     float percentage;
     FILE *fptr;
-
-
-
 //***************************************************************
 //                   FUNCTION USED IN PROJECT
 //****************************************************************
-void login(){
-int i,num,count=0;
+
+void login()
+{
+     int num,count=0;
 	do
 	{
 		system("cls");
-			if(count>0)
-	{
-	    printf("-------------------\n");
-		printf("****Invalid pin****\n");
-		printf("-------------------\n");
-	}
-	printf("Enter the password:\n");
+        if(count>0)
+            {
+                printf("-------------------\n");
+                printf("****Invalid pin****\n");
+                printf("-------------------\n");
+            }
+	printf("Enter the password:   \n");
 	scanf("%d",&num);
 	count++;
-	}
-	while((num!=1234)&&(count<3));   //boundary value//
+	}while((num!=1234)&&(count<3));   //boundary value//
 	if((count<=3) && (num==1234))
-	{
-		system("cls");
-		printf("-------------------\n");
-		printf("***Welcome***\n");
-		printf("-------------------\n");
-	}
+        {
+            system("cls");
+            printf("--------------------------\n");
+            printf("   ***Welcome***\n");
+            printf("--------------------------\n");
+            option();
+        }
 	else //if(count<3)
-	{
-	    printf("----------------------------\n");
-		printf("****Invalid login attempt***\n");
-		printf("----------------------------\n");
-		exit(0);
-	}
+        {
+            printf("----------------------------\n");
+            printf("****Invalid login attempt***\n");
+            printf("----------------------------\n");
+            exit(0);
+        }
 	//choice();
-
 }
 
-
-    void calculate()
+void option()
+{
+    int choice;
+    char inp;
+    printf("\nPress 1 to enter record of student\n");
+    printf("Press 2 to search existing student\n");
+    printf("Press 3 to exit\n");
+    printf("Enter you choice : ");
+    scanf("%d",&choice);
+    switch(choice)
     {
-         percentage=s_marks+e_marks+m_marks+n_marks/4.0;
-
-         if(percentage>=60)
-            grade='A';
-        else if(percentage>=50 && percentage<60)
-            grade='B';
-        else if(percentage>=30 && percentage<50)
-            grade='C';
-        else
-            grade='F';
-    }
-    void choice()
-    {
-        int p;
-        printf("\n1.Enter records");
-        printf("\n2.Show Data");
-        printf("\n3.Show results");
-        printf("\nEnter your choice:");
-        scanf("%d",&p);
-        printf("\n");
-        switch(p)
-        {
         case 1:
             {
-                printf("---------------------------\n");
-                printf("    ***Enter records***\n");
-                printf("---------------------------\n");
+                get_info();
                 break;
             }
-
-              case 2:
+        case 2:
             {
-                printf("\nShow Data");
+                printf("Enter student ID : ");
+                scanf("%d",&id);
                 break;
             }
-              case 3:
+        case 3:
             {
-                printf("\nShow results");
+                printf("\n***Thank you so much***");
+                exit(0);
                 break;
             }
-            default:
+        default:
             {
-                printf("\nInvalid input");
+                case_default();
                 break;
             }
-        }
-    }
-    void entry()
-    {
-        printf("Enter the id no of student:");
-        scanf("%d",&id);
-        printf("Enter the name of student: ");
-        gets(name);
-
     }
 
-    void getdata()
-    {
+}
+void case_default()
+{
+    char inp;
+    printf("\n--Invalid Input--\n");
+    printf("\nDo you want to exit or not (Y/N):");
+    scanf("%s",&inp);
+    if(inp =='Y' || inp =='y' )
+     {
+         exit(0);
+     }
+     else if(inp == 'N' || inp == 'n')
+     {
+         option();
+     }
+     else
+     {
+         //printf("\nInvalid selection");
+         case_default();
+     }
+}
+
+ void get_info()
+ {
+     printf("\nEnter the id no of student:");
+     scanf("%d",&id);
+     printf("Enter the name of student:");
+     gets(a);
+     gets(name);
+     getdata();
+
+ }
+
+
+
+void getdata()
+{
         printf("\n");
-        printf("-------------------------------------\n");
+        printf("----------------------------------------\n");
         printf("Enter the marks obtained in Science: ");
         scanf("%d",&s_marks);
         printf("Enter the marks obtained in English: ");
@@ -128,84 +138,70 @@ int i,num,count=0;
         scanf("%d",&m_marks);
         printf("Enter the marks obtained in Nepali: ");
         scanf("%d",&n_marks);
-        printf("-------------------------------------\n");
-        calculate();
+        printf("----------------------------------------\n");
+        percentage=s_marks+e_marks+m_marks+n_marks/4.0;
 
-    }
-    void showdata()
-    {
+
+
+        if(percentage>=60)
+            grade='A';
+        else if(percentage>=50 && percentage<60)
+            grade='B';
+        else if(percentage>=30 && percentage<50)
+            grade='C';
+        else
+            grade='F';
+        showdata();
+
+}
+
+void showdata()
+{
         printf("\nID of a student: %d",id);
-       printf("\nName of a student: %s",name);
-      //  printf("\nAddress of a student: %c",address);
+        printf("\nName of a student: %s\n",name);
         printf("\nMarks obtained in Science: %d",s_marks);
-        printf("\nMarks obtained in English: %d",e_marks);
-        printf("\nMarks obtained in Math: %d",m_marks);
-        printf("\nMarks obtained in Nepali: %d",n_marks);
-        printf("\nGrade of a student is %c",grade);
-       // printf("\nDo you want to continue Y/N?");
-
-
-    }
-
-    //***************************************************************
-   //      function to display result menu
-  //****************************************************************
-
-    void result()
-    {
-        int ans,rno;
-        char ch;
-
-        printf("\n\nRecord Menu");
-        printf("\n\n\n1. Class Result\n\n2. Student Entrance Exam Record System\n\n3.Back to Main Menu");
-        printf("\n\n\nEnter Choice (1/2)? ");
-        switch(ans)
+        if(s_marks>=32)
         {
-     case 1 :
-       // class_result();
-        break;
-    case 2 :
-    {
-        do
-       {
+            printf(" (PASS)");
+        }
+        else
+        {
+            printf(" (FAIL)");
+        }
+        printf("\nMarks obtained in English: %d",e_marks);
+        if(e_marks>=32)
+        {
+            printf(" (PASS)");
+        }
+        else
+        {
+            printf(" (FAIL)");
+        }
+        printf("\nMarks obtained in Math: %d",m_marks);
+        if(m_marks>=32)
+        {
+            printf(" (PASS)");
+        }
+        else
+        {
+            printf(" (FAIL)");
+        }
+        printf("\nMarks obtained in Nepali: %d",n_marks);
+        if(n_marks>=32)
+        {
+            printf(" (PASS)");
+        }
+        else
+        {
+            printf(" (FAIL)");
+        }
+        //printf("\nPercentage is : %f",percentage);
+        printf("\nGrade of a student is %c",grade);
 
-            char ans;
-            printf("\n\nEnter roll no Of Student : ");
-            scanf("%d",rno);
-            printf("\n\nDo you want to See More Result (y/n)?");
-            scanf("%d",ans);
-       }
-       while(ans=='y'||ans=='Y');
-
-       break;
-    }
-    case 3:
-        break;
-    default:
-        printf("\a");
-       }
-   }
-
-
-    /*void add_student()
-  {
-      // *fptr=fopen("C:\Users\Ghost\Desktop\student.txt","a");
-    //   *fptr.getdata();
-     //  fappend((char*),sizeof(*fptr));
-       fclose;
-       printf("\nStudent records has been created");
-       getch();
-   }*/
-
-main(){
- login();
- choice();
-    entry();
-    getdata();
-    showdata();
-    result();}
+}
 
 
-
-
-
+main()
+{
+    login();
+}
